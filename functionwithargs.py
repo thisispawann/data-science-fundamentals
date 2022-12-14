2ae0f6ea4c252588ddf3b3d3b3ce88796fcd65a3
thisispawann <pawanstha.6964@gmail.com>
2022-12-14 20:33:42 +0545

    function argument

---
+++ functionwithargs.py
@@ -0,0 +1,9 @@
+def add_num(a, b):
+    sum = a + b
+    return sum
+
+num1 = int(input('Enter first number: '))
+num2 = int(input('Enter second number: '))
+
+sum = add_num(num1, num2)
+print('The sum is: ', sum23)
