19c72ef5580b30c6a22535273e02c1dc4e9758e8
thisispawann <pawanstha.6964@gmail.com>
2022-12-15 11:10:13 +0545

    modules and package

---
+++ modulesandpackage.py
@@ -0,0 +1,31 @@
+# import standard math module
+
+import math
+print('the value of pi is: ', math.pi)
+
+# import module by renaming it
+
+import math as m
+
+print(m.pi)
+
+# python from..import statement
+# we can import specific names from a module without importing the module as a whole
+
+from math import pi
+
+print(pi)
+
+# import all names
+from math import *
+
+print('the value of pi is: ', pi)
+
+from calculate import *
+
+a = 10
+
+b = 8
+
+result = mul(a, b)
+print(result)
