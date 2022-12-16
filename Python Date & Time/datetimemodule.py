32564c7cf8c13429fb230120a9d076a68d50f916
thisispawann <pawanstha.6964@gmail.com>
2022-12-16 09:20:14 +0545

    datetime module

---
+++ Python Date & Time/datetimemodule.py
@@ -0,0 +1,18 @@
+import datetime
+
+dt = datetime.datetime.now()
+print(dt)
+
+'''
+current date
+'''
+import datetime
+
+date_today = datetime.date.today()
+print(date_today)
+
+# another way to deal with current date
+from datetime import date
+
+today = date.today()
+print('Current date is: ', today)
